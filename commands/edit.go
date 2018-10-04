744342d80743ecd1b3ddbd68bf2c8d081789d761
Jordan Knott <jordan@jordanthedev.com>
2018-10-04 15:12:32 -0500

    add description and allow negative index numbers in edit cmd

--- commands/edit.go
+++ commands/edit.go
@@ -13,11 +13,23 @@ import (
     "github.com/spf13/cobra"
 )
 
+const description = `Edit a frame.
+
+You can specific which frame to edit through either its ID or relative position in the index.
+For example if you wanted to get the last frame you edited
+you would do ` + "`chrono edit -- -1`" + `.
+
+The '--' is needed so that the negative number is not parsed
+as a flag.
+
+If you wanted to get the earliest frame,
+you would do ` + "`chrono edit 1`" + `.`
+
 func newEditCmd() *cobra.Command {
     return &cobra.Command{
         Use: "edit [index]",
-        Short: "edit a frame",
-        Long: "edit a frame",
+        Short: "Edit a frame",
+        Long: description,
         Args: cobra.ExactArgs(1),
         Run: func(cmd *cobra.Command, args []string) {
             framesPath := chronolib.GetAppFilePath("frames", "")
@@ -34,7 +46,12 @@ func newEditCmd() *cobra.Command {
                     os.Exit(-1)
                 }
             } else {
-                targetIndex = len(data.Frames) - index
+                if index < 0 {
+                    targetIndex = len(data.Frames) + index
+                } else {
+                    targetIndex = len(data.Frames) - index
+                }
+
                 target, err = data.GetFrameByIndex(targetIndex)
                 if err != nil {
                     fmt.Println("No frame found at that index")
