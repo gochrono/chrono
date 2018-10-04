6a0504798b9c7cf3a82ca224b01fd863337d7bfb
Jordan Knott <jordan@jordanthedev.com>
2018-10-04 16:20:08 -0500

    add version check command

---
+++ chronolib/githubapi.go
@@ -0,0 +1,76 @@
+package chronolib
+
+import "time"
+
+type GithubLatestRelease struct {
+	URL             string    `json:"url"`
+	HTMLURL         string    `json:"html_url"`
+	AssetsURL       string    `json:"assets_url"`
+	UploadURL       string    `json:"upload_url"`
+	TarballURL      string    `json:"tarball_url"`
+	ZipballURL      string    `json:"zipball_url"`
+	ID              int       `json:"id"`
+	NodeID          string    `json:"node_id"`
+	TagName         string    `json:"tag_name"`
+	TargetCommitish string    `json:"target_commitish"`
+	Name            string    `json:"name"`
+	Body            string    `json:"body"`
+	Draft           bool      `json:"draft"`
+	Prerelease      bool      `json:"prerelease"`
+	CreatedAt       time.Time `json:"created_at"`
+	PublishedAt     time.Time `json:"published_at"`
+	Author          struct {
+		Login             string `json:"login"`
+		ID                int    `json:"id"`
+		NodeID            string `json:"node_id"`
+		AvatarURL         string `json:"avatar_url"`
+		GravatarID        string `json:"gravatar_id"`
+		URL               string `json:"url"`
+		HTMLURL           string `json:"html_url"`
+		FollowersURL      string `json:"followers_url"`
+		FollowingURL      string `json:"following_url"`
+		GistsURL          string `json:"gists_url"`
+		StarredURL        string `json:"starred_url"`
+		SubscriptionsURL  string `json:"subscriptions_url"`
+		OrganizationsURL  string `json:"organizations_url"`
+		ReposURL          string `json:"repos_url"`
+		EventsURL         string `json:"events_url"`
+		ReceivedEventsURL string `json:"received_events_url"`
+		Type              string `json:"type"`
+		SiteAdmin         bool   `json:"site_admin"`
+	} `json:"author"`
+	Assets []struct {
+		URL                string    `json:"url"`
+		BrowserDownloadURL string    `json:"browser_download_url"`
+		ID                 int       `json:"id"`
+		NodeID             string    `json:"node_id"`
+		Name               string    `json:"name"`
+		Label              string    `json:"label"`
+		State              string    `json:"state"`
+		ContentType        string    `json:"content_type"`
+		Size               int       `json:"size"`
+		DownloadCount      int       `json:"download_count"`
+		CreatedAt          time.Time `json:"created_at"`
+		UpdatedAt          time.Time `json:"updated_at"`
+		Uploader           struct {
+			Login             string `json:"login"`
+			ID                int    `json:"id"`
+			NodeID            string `json:"node_id"`
+			AvatarURL         string `json:"avatar_url"`
+			GravatarID        string `json:"gravatar_id"`
+			URL               string `json:"url"`
+			HTMLURL           string `json:"html_url"`
+			FollowersURL      string `json:"followers_url"`
+			FollowingURL      string `json:"following_url"`
+			GistsURL          string `json:"gists_url"`
+			StarredURL        string `json:"starred_url"`
+			SubscriptionsURL  string `json:"subscriptions_url"`
+			OrganizationsURL  string `json:"organizations_url"`
+			ReposURL          string `json:"repos_url"`
+			EventsURL         string `json:"events_url"`
+			ReceivedEventsURL string `json:"received_events_url"`
+			Type              string `json:"type"`
+			SiteAdmin         bool   `json:"site_admin"`
+		} `json:"uploader"`
+	} `json:"assets"`
+}
--- commands/version.go
+++ commands/version.go
@@ -2,7 +2,10 @@ package commands
 
 import (
     "fmt"
+    "encoding/json"
     "github.com/spf13/cobra"
+    "gopkg.in/resty.v1"
+    "github.com/jordanknott/chrono/chronolib"
 )
 
 var (
@@ -10,6 +13,8 @@ var (
 	commit  = "none"
 	date    = "unknown"
 )
+
+const latestReleaseUrl = "https://api.github.com/repos/jordanknott/chrono/releases/latest"
 
 var banner = `   _____ _
   / ____| |
@@ -19,13 +24,40 @@ var banner = `   _____ _
   \_____|_| |_|_|  \___/|_| |_|\___/`
 
 func newVersionCmd() *cobra.Command {
-    return &cobra.Command{
+    versionCmd := &cobra.Command{
         Use: "version",
         Short: "Prints the version then exits",
         Long: "Prints the version then exits",
         Run: func(cmd *cobra.Command, args []string) {
             fmt.Println(banner)
             fmt.Printf("\nversion: %s\ncommit: %s\nbuilt: %s\n", version, commit, date)
+        },
+    }
+
+    versionCmd.AddCommand(newVersionCheckCmd())
+    return versionCmd
+}
+
+func newVersionCheckCmd() *cobra.Command {
+    return &cobra.Command{
+        Use: "check",
+        Short: "Checks if there is a new version",
+        Long: "Checks if there is a new version release on GitHub",
+        Run: func(cmd *cobra.Command, args []string) {
+            resp, err := resty.R().Get(latestReleaseUrl)
+            if err != nil { panic(err) }
+            if resp.StatusCode() == 200 {
+                var latestRelease chronolib.GithubLatestRelease
+                json.Unmarshal(resp.Body(), &latestRelease)
+                if version != latestRelease.TagName {
+                    fmt.Printf("Found a new release [%s]!\nVisit %s and download the latest version\n", latestRelease.TagName, latestRelease.HTMLURL)
+                } else {
+                    fmt.Println("You are on the latest version!")
+                }
+            } else {
+                fmt.Println("No releases found.")
+            }
+
         },
     }
 }
