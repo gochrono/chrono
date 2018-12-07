26b96e58aeeff0f17ec2a22ee4ba77bfeb4c073e
Jordan Knott <jordan@jordanthedev.com>
2018-12-06 20:33:30 -0600

    begin config system

--- chronolib/config.go
+++ chronolib/config.go
@@ -1,10 +1,15 @@
 package chronolib
 
 import (
+	"github.com/gookit/config"
+	"github.com/gookit/config/toml"
 	"github.com/kirsle/configdir"
 	"os"
 	"path/filepath"
 )
+
+// ChronoConfigFilename is the config's expected filename
+const ChronoConfigFilename = "config.toml"
 
 // ChronoAppConf is the name of the app's data directory
 const ChronoAppConf = "chrono"
@@ -26,4 +31,53 @@ func GetAppFilePath(fileName string, customConfDir string) string {
 		panic(err)
 	}
 	return filepath.Join(appConfDir, fileName)
+}
+
+// GetCorrectConfigDirectory returns the correct config path where customConfigPath > Env Variable > Default value
+func GetCorrectConfigDirectory(customConfigPath string) string {
+	var appConfDir = configdir.LocalConfig(ChronoAppConf)
+
+	if os.Getenv(ChronoConfDirEnvName) != "" {
+		appConfDir = os.Getenv(ChronoConfDirEnvName)
+	}
+	if customConfigPath != "" {
+		appConfDir = customConfigPath
+	}
+	return appConfDir
+}
+
+// EnsureConfigDirExists makes sure the configDirectory path exists
+func EnsureConfigDirExists(configDirectory string) error {
+	return configdir.MakePath(configDirectory)
+}
+
+// GetConfig loads the a confi file if it exists and returns a ChronoConfig struct
+func GetConfig(configDirectory string) ChronoConfig {
+	configFile := filepath.Join(configDirectory, ChronoConfigFilename)
+	config.WithOptions(config.ParseEnv)
+	config.AddDriver(toml.Driver)
+	config.SetDecoder(config.Toml, toml.Decoder)
+
+	err := config.LoadExists(configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	storage, ok := config.String("general.storage")
+	if !ok {
+		storage = "msgpack"
+	}
+	generalConfig := chronoGeneralConfig{storage}
+	return ChronoConfig{configDirectory, generalConfig}
+}
+
+type chronoGeneralConfig struct {
+	Storage string
+}
+
+// ChronoConfig contains the currently used config dir path as well as any configuration options
+// stored in config.toml
+type ChronoConfig struct {
+	ConfigDir     string
+	GeneralConfig chronoGeneralConfig
 }
--- go.mod
+++ go.mod
@@ -1,12 +1,14 @@
 module github.com/gochrono/chrono
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/SaidinWoT/timespan v0.0.0-20160403210742-a3d8e4741124
 	github.com/davecgh/go-spew v1.1.1
 	github.com/dustin/go-humanize v1.0.0
 	github.com/fatih/color v1.7.0
 	github.com/golang/protobuf v1.2.0
 	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf
+	github.com/gookit/config v1.0.11
 	github.com/inconshreveable/mousetrap v1.0.0
 	github.com/jinzhu/now v0.0.0-20180511015916-ed742868f2ae
 	github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f
@@ -14,6 +16,7 @@ require (
 	github.com/mattn/go-colorable v0.0.9
 	github.com/mattn/go-isatty v0.0.4
 	github.com/pmezard/go-difflib v1.0.0
+	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3
 	github.com/stretchr/testify v1.2.2
 	github.com/vmihailenco/msgpack v4.0.0+incompatible
@@ -22,4 +25,5 @@ require (
 	golang.org/x/text v0.3.0
 	google.golang.org/appengine v1.2.0
 	gopkg.in/resty.v1 v1.9.1
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
